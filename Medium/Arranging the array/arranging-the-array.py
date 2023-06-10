7d7bc8b42d84f167bfc3423ddb87ce98a86034b0
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-06-10 08:40:27 +0600

    Added Solution - GfG to GitHub

---
+++ Medium/Arranging the array/arranging-the-array.py
@@ -0,0 +1,53 @@
+
+from typing import List
+
+class Solution:
+    def Rearrange(self, n : int, arr : List[int]) -> None:
+        neg = []
+        pos = []
+        
+        for i in arr:
+            if (i < 0):
+                neg.append(i)
+            else:
+                pos.append(i)
+        
+        neg.extend(pos)
+        
+        for i in range(n):
+            arr[i] = neg[i]
+            
+        return arr
+        
+
+#{ 
+ # Driver Code Starts
+
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        n = int(input())
+        
+        
+        arr=IntArray().Input(2)
+        
+        obj = Solution()
+        obj.Rearrange(n, arr)
+        
+        print(*arr, end=' ')
+        print()
+
+# } Driver Code Ends
