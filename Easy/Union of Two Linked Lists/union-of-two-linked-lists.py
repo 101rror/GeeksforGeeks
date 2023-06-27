8ee4c369750a829e38426c69d7903f9b2cc4778f
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-06-27 12:33:10 +0600

    Added Solution - GfG to GitHub

---
+++ Easy/Union of Two Linked Lists/union-of-two-linked-lists.py
@@ -0,0 +1,72 @@
+#User function Template for python3
+
+class Solution:
+    def union(self, head1,head2):
+        ans = set()
+        
+        while(head1):
+            ans.add(head1.data)
+            head1 = head1.next
+        while(head2):
+            ans.add(head2.data)
+            head2 = head2.next
+            
+            
+        ans = sorted(ans)
+        res = linkedList()
+        
+        for i in ans:
+            res.insert(i)
+                
+        return res.head
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class linkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+    
+    def insert(self,data):
+        if self.head is None:
+            self.head = Node(data)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(data)
+            self.tail = self.tail.next
+
+def printList(head):
+    while head:
+        print(head.data,end=' ')
+        head=head.next
+        
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        
+        n1 = int(input())
+        arr1 = [int(x) for x in input().split()]
+        ll1 = linkedList()
+        for i in arr1:
+            ll1.insert(i)
+        
+        n2 = int(input())
+        arr2 = [int(x) for x in input().split()]
+        ll2 = linkedList()
+        for i in arr2:
+            ll2.insert(i)
+        
+        ob = Solution()
+        printList(ob.union(ll1.head,ll2.head))
+        print()
+
+# } Driver Code Ends
