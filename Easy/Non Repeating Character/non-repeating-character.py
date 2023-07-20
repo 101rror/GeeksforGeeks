3fbb09031902f4c9cd4b34492a1ea96123f08a7e
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-07-20 19:53:11 +0600

    Added Solution - GfG to GitHub

---
+++ Easy/Non Repeating Character/non-repeating-character.py
@@ -0,0 +1,46 @@
+#User function Template for python3
+
+class Solution:
+    def nonrepeatingCharacter(self,s):
+        n = len(s)
+        
+        for i in range(n):
+            if(s.count(s[i]) == 1):
+                return s[i]
+                break
+        else:
+            return "$"
+                    
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        s=str(input())
+        obj = Solution()
+        ans=obj.nonrepeatingCharacter(s)
+        if(ans!='$'):
+            print(ans)
+        else:
+            print(-1)
+            
+# } Driver Code Ends
