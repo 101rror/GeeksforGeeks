2c8efb2e9f49770c5bc446a5d258a626224bf23c
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-09-15 21:33:34 +0600

    Added Solution - GfG to GitHub

---
+++ School/Print alternate elements of an array/print-alternate-elements-of-an-array.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+
+# arr is the array
+# n is the number of elements in array
+def printAl(arr,n):
+    for i in range(0, n, 2):
+        print(arr[i], end = " ")
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=="__main__":
+    t=int(input())
+    for i in range(t):
+        n=int(input())
+        arr=list(map(int,input().split()))
+        printAl(arr,n)
+        print()
+# } Driver Code Ends
