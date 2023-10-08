5b6b1c774e077cb603eed230fe5f10be419bffa9
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-10-08 10:56:01 +0600

    Added Solution - GfG to GitHub

---
+++ Easy/Insert in a Sorted List/insert-in-a-sorted-list.py
@@ -0,0 +1,72 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+        
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.prev=self.head
+    
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.prev = self.head
+        else:
+            self.prev.next = new_node
+            self.prev = self.prev.next
+
+def printList(head):
+    while head:
+        print(head.data, end=' ')
+        head = head.next
+    print()
+
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def sortedInsert(self, head, key):
+        n = Node(key)
+        if(head.data > key):
+            n.next = head
+            head = n
+            return head
+            
+        a = head
+        
+        while(a.next and a.next.data < key):
+            a = a.next
+            
+        temp = a.next
+        a.next = n
+        a.next.next = temp
+            
+        return head
+
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        n = int(input())
+        
+        a = LinkedList()
+        nodes = list(map(int, input().strip().split()))
+        for x in nodes:
+            a.append(x)
+        
+        key=int(input())
+        h=Solution().sortedInsert(a.head,key)
+        printList(h)
+
+# } Driver Code Ends
