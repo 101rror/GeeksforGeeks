92f2815cbb39ceaec436760e94d0430ba67279af
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-12-19 11:26:06 +0600

    Added Solution - GfG to GitHub

---
+++ Easy/Rightmost different bit/rightmost-different-bit.py
@@ -0,0 +1,60 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+
+
+    
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find the first position with different bits.
+    def posOfRightMostDiffBit(self,m,n):
+        ans = m ^ n
+        
+        if(ans == 0):
+            return -1
+        
+        x = 1
+        
+        while ans:
+            if(ans % 2):
+                return x
+            ans >>= 1
+            x += 1
+        
+
+#{ 
+ # Driver Code Starts.
+    
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        mn=[int(x) for x in input().strip().split()]
+        m=mn[0]
+        n=mn[1]
+        ob=Solution()
+        print(math.floor(ob.posOfRightMostDiffBit(m,n)))
+        
+        
+        
+        
+        T-=1
+    
+    
+
+
+
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
