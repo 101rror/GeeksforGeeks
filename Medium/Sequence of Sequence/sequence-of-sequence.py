94a452d2f4ecaaa66ff16b2897a43a781cf3777a
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-01-16 21:59:00 +0600

    Added Solution - GfG to GitHub

---
+++ Medium/Sequence of Sequence/sequence-of-sequence.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+
+class Solution:
+    def numberSequence(self, m, n):
+        if m < n:
+            return 0
+        if n == 0:
+            return 1
+            
+        x = self.numberSequence(m / 2, n - 1)
+        y = self.numberSequence(m - 1, n)
+        
+        return x + y
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range (t):
+        arr = input().split()
+        m = int(arr[0])
+        n = int(arr[1])
+        
+        ob = Solution()
+        print(ob.numberSequence(m, n))
+# } Driver Code Ends
