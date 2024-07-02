d8206fbf394883deae18f4d0f260a027c153268f
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-07-02 17:20:47 +0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Linked list of strings forms a palindrome/linked-list-of-strings-forms-a-palindrome.py
@@ -0,0 +1,64 @@
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+'''
+def compute(head): 
+    newstr = ''
+    
+    while head:
+        newstr += head.data
+        head = head.next
+        
+    return newstr == (newstr[::-1])
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+#contributed by RavinderSinghPB
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Llist:
+
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data, tail):
+        node = Node(data)
+
+        if not self.head:
+            self.head = node
+            return node
+
+        tail.next = node
+        return node
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for tcs in range(t):
+
+        n1 = int(input())
+        arr1 = input().split()
+        ll1 = Llist()
+        tail = None
+        for nodeData in arr1:
+            tail = ll1.insert(nodeData, tail)
+
+        if compute(ll1.head):
+            print('true')
+        else:
+            print('false')
+
+# } Driver Code Ends
