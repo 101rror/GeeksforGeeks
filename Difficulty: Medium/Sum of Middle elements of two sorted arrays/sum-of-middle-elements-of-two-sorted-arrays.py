f4e6ea49db5538bf7cc5f0939b5ee1757d91c4c9
Mr. Error <122160056+101rror@users.noreply.github.com>
2024-08-12 21:37:10 +0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of Middle elements of two sorted arrays/sum-of-middle-elements-of-two-sorted-arrays.py
@@ -0,0 +1,46 @@
+#User function Template for python3
+
+class Solution:
+    def sum_of_middle_elements(self, arr1, arr2):
+        arr1 += arr2
+        arr1.sort()
+        
+        n = len(arr1)
+        
+        return arr1[n // 2 - 1] + arr1[n // 2]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import sys
+
+input = sys.stdin.read
+
+
+def main():
+    input_lines = input().strip().split("\n")
+    t = int(input_lines[0])
+
+    index = 1
+    results = []
+    while t > 0:
+        arr = list(map(int, input_lines[index].strip().split()))
+        brr = list(map(int, input_lines[index + 1].strip().split()))
+        index += 2
+
+        solution = Solution()
+        res = solution.sum_of_middle_elements(arr, brr)
+        results.append(res)
+
+        t -= 1
+
+    for result in results:
+        print(result)
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
