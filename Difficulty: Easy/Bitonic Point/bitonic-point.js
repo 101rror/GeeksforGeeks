2008704a72897d11907385678ced6b2656fc83e7
Error <122160056+101rror@users.noreply.github.com>
2024-09-21 01:47:02 +0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bitonic Point/bitonic-point.js
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let input_ar0 = readLine().split(' ').map(x=>parseInt(x));
+        let n = input_ar0[0];
+        let arr = new Array(n);
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        for(let i=0;i<n;i++)
+            arr[i] = input_ar1[i];
+        let obj = new Solution();
+        let res = obj.findMaximum(arr,n);
+        console.log(res);
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+
+/**
+ * @param {number[]} arr
+ * @param {number} N
+ * @returns {number}
+*/
+
+class Solution {
+    
+    findMaximum(arr,n)
+    {
+        return Math.max(...arr);
+    }
+}
