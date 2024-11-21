4a1dcb77e576aa31e523375f6d0bfb0d4d8eba9a
Error <122160056+101rror@users.noreply.github.com>
2024-11-21 11:01:09 +0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Stock Buy and Sell – Multiple Transaction Allowed/stock-buy-and-sell-multiple-transaction-allowed.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def maximumProfit(self, prices) -> int:
+        n = len(prices)
+        profit = 0
+        for i in range(1, n):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
+                
+        return profit
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().strip().split()))
+        solution = Solution()
+        res = solution.maximumProfit(arr)
+        print(res)
+
+# } Driver Code Ends
