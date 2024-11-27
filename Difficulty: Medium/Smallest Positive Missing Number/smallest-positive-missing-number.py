8af6639a4be2290aa948d92972db3535b24e0b13
Error <122160056+101rror@users.noreply.github.com>
2024-11-27 10:03:28 +0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest Positive Missing Number/smallest-positive-missing-number.py
@@ -0,0 +1,37 @@
+#User function Template for python3
+
+class Solution:
+    def missingNumber(self,arr):
+        n = len(arr)
+        
+        for i in range(n):
+            while 1 <= arr[i] <= n and arr[arr[i] - 1] != arr[i]:
+                arr[arr[i] - 1], arr[i] = arr[i], arr[arr[i] - 1]
+                
+        for i in range(n):
+            if arr[i] != i + 1:
+                return i + 1
+                
+        return n + 1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    T = int(input())
+    while (T > 0):
+        arr = [int(x) for x in input().strip().split()]
+        ob = Solution()
+        print(ob.missingNumber(arr))
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
