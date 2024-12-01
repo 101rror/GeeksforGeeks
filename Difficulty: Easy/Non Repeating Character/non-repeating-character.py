b3b258316c797a1c33f42d7dba2886e8e81120f9
Error <122160056+101rror@users.noreply.github.com>
2024-12-01 12:26:31 +0600

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.py
@@ -0,0 +1,48 @@
+from collections import Counter
+
+class Solution:
+    def nonRepeatingChar(self,s):
+        counter = Counter(s)
+        
+        for key, val in counter.items():
+            if val == 1:
+                return key
+        
+        return '$'
+    
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        s = str(input())
+        obj = Solution()
+        ans = obj.nonRepeatingChar(s)
+        if (ans != '$'):
+            print(ans)
+        else:
+            print(-1)
+
+        print("~")
+
+# } Driver Code Ends
