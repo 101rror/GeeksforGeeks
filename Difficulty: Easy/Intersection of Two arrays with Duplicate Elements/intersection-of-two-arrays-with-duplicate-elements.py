2c273a3d4d6dc0184b2b17bf21c41c46b1ada562
Error <122160056+101rror@users.noreply.github.com>
2024-12-29 21:27:30 +0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Intersection of Two arrays with Duplicate Elements/intersection-of-two-arrays-with-duplicate-elements.py
@@ -0,0 +1,39 @@
+
+class Solution:
+    def intersectionWithDuplicates(self, a, b):
+        ans = list(set(a) & set(b))
+        
+        return ans
+        
+#{ 
+ # Driver Code Starts
+#Position this line where user code will be pasted.
+t = int(input().strip())
+while t > 0:
+    t -= 1
+    # Read first array
+    a = list(map(int, input().strip().split()))
+
+    # Read second array
+    b = list(map(int, input().strip().split()))
+
+    #input()  # to consume the empty line
+
+    # ADD Solution initialization
+    sln = Solution()
+
+    # Assuming numberofElementsInIntersection function is defined in Solution
+    res = sln.intersectionWithDuplicates(a, b)
+
+    # Sort the result
+    res.sort()
+
+    # Print the result
+    if not res:
+        print("[]")
+    else:
+        print(" ".join(map(str, res)))
+
+    print("~")
+
+# } Driver Code Ends
