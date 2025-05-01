1cd7e2f97cf0da3a1fc05c2984616d87da5a7717
Error <mr.101rror@gmail.com>
2025-05-01 06:01:28 +0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Pascal Triangle/pascal-triangle.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+
+class Solution:
+    def nthRowOfPascalTriangle(self,n):
+        mod = 10 ** 9 + 7
+        arr = [1]
+        
+        if n == 1:
+            return arr
+        else:
+            for i in range(1, n):
+                ans = [1]
+                for j in range(i - 1):
+                    ans.append((arr[j + 1] + arr[j]) % mod)
+                    
+                ans.append(1)
+                arr = ans
+                
+            return ans
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        ob = Solution()
+        ans = ob.nthRowOfPascalTriangle(n)
+        for x in ans:
+            print(x, end=" ")
+        print()
+        tc = tc - 1
+        print("~")
+# } Driver Code Ends
