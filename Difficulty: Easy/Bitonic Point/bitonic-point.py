560dba15748add992b07b64ef2b5fb0c5a4cd2c0
Error <mr.101rror@gmail.com>
2025-05-02 09:40:46 +0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bitonic Point/bitonic-point.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+class Solution:
+
+	def findMaximum(self, arr):
+		return max(arr)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.findMaximum(arr)
+        print(ans)
+        tc -= 1
+        print("~")
+
+# } Driver Code Ends
